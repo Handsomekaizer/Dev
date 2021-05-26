494f1c9161a6d20b14ab5ce653554ab51bf16088
Shiristy60 <shiristykotnala1.1@gmail.com>
2021-05-26 20:55:35 +0530

    Final changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import DeviceDetector from "device-detector-js"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const deviceDetector = new DeviceDetector()
+const userAgent = navigator.userAgent
+const device = deviceDetector.parse(userAgent)
+
+const App = () => {
+	const [deviceName, setDeviceName] = useState("")
+	const [OS, setOS] = useState("")
+	const [location, setLocation] = useState({})
+
+	const getOS = () => {
+		const osName = device.os.name
+		const osVersion = device.os.version
+
+		const userOS = `${osName}, ${osVersion}`
+		setOS(userOS)
+	}
+	const getLocation = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition)
+		}
+	}
+
+	const getDevice = () => {
+		const deviceType = device.device.type
+		const deviceBrand = device.device.brand
+		const deviceModel = device.device.model
+
+		setDeviceName(`${deviceType} ${deviceBrand} ${deviceModel}`)
+	}
+
+	const showPosition = (position) => {
+		const loc = {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude,
+		}
+		setLocation(loc)
+	}
+
+	useEffect(() => {
+		getOS()
+		getLocation()
+		getDevice()
+	})
+
+	return (
+		<div className='main-container'>
+			<div className='content'>
+				<p className='info'>
+					Your device is <span className='info__specs'>{deviceName}</span>
+					<br />
+					You are using <span className='info__specs'>{OS}</span>
+				</p>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
